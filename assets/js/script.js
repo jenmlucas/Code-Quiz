48cbe9dc02075e96d0e4f96e0e8ad52e6df79ef7
jenmlucas <jenmlucas1@gmail.com>
2021-10-03 15:58:16 -0700

    added correct and incorrect labels, with a time subtraction for incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ console.log(startBtn);
 var timerBegins = document.getElementById("timerbegins");
 var questionText = document.getElementById("questionText");
 var questionAnswers = document.getElementById("questionAnswers");
-
+var newTime = document.getElementById("newTime");
 //var answerArrayQ1= ["Answer 1", "Answer 2", "Answer 3", "Answer 4"];
 //var questionArray= ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"]
 
@@ -33,74 +33,81 @@ var quizList = [
     {
         question: "Question 5:",
         choices: ["A5", "B5", "C5", "D5"],
-        answer: "D6"
+        answer: "D5"
     }
 ];
 //WHEN I click the start button
 //THEN a timer starts and I am presented with a question
-function startGame () {
-     console.log("button click");
-     countdown();
-     displayQuestions();
-     displayAnswers();
+function startGame() {
+    console.log("button click");
+    countdown();
+    displayQuestions();
+    displayAnswers();
 };
 
-function countdown () {
-    var timeLeft = 60;
+var timeLeft = 60;
 
-var timeInterval = setInterval (function() {
-  if (timeLeft > 1) {
-      timerBegins.textContent = timeLeft + ' seconds remaining'; 
-      timeLeft --; 
-  } else if (timeLeft === 1) {
-      timerBegins.textContent = timeLeft + ' second remaining'; 
-      timeLeft --;
-  } else {
-      timerBegins.textContent = "";
-      clearInterval(timeInterval);
-     
-  }
-}, 1000); 
+function countdown() {
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerBegins.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerBegins.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            timerBegins.textContent = "";
+            clearInterval(timeInterval);
+    
+        }
+    }, 1000);
 };
 
 //WHEN I answer a question
 //THEN I am presented with another question
-function displayQuestions () {
+function displayQuestions() {
     //createElement that serves as the box where the question will go, can use classes and ID. Update text content, use an array of questions
     //for (var i = 0; i < questionArray.length; i++){
-        //reset the question area
-    questionText.innerHTML= "";
-   var questionContent = document.createElement("section");
-   questionContent.textContent = quizList[currentQuestion].question;
-   questionText.appendChild(questionContent);
+    //reset the question area
+    questionText.innerHTML = "";
+    var questionContent = document.createElement("section");
+    questionContent.textContent = quizList[currentQuestion].question;
+    questionText.appendChild(questionContent);
 };
 
-function displayAnswers () {
-    questionAnswers.innerHTML= "";
-    for (var i = 0; i < quizList[currentQuestion].choices.length; i++){
+function displayAnswers() {
+    questionAnswers.innerHTML = "";
+    for (var i = 0; i < quizList[currentQuestion].choices.length; i++) {
         var answersContent = document.createElement("section");
         answersContent.textContent = quizList[currentQuestion].choices[i];
-        answersContent.addEventListener("click", authenticate)
+        answersContent.addEventListener("click", authenticate);
         questionAnswers.appendChild(answersContent);
     }
-}; 
+};
 // set for multiple questions, pass in different question in display questions, loop to display questions, array to answers 
 function authenticate() {
     console.log("button clicked", this.textContent);
 
     var answer = quizList[currentQuestion].answer;
     if (this.textContent === answer) {
-        textContent.alert("correct answer")
-        
-    } else {
-        alert("Incorrect Answer");
+        newTime.textContent = "Correct";
+        setTimeout(function () {
+            newTime.textContent = "";
+            displayAnswers();
+            displayQuestions();
+        }, 1000)
     }
-    currentQuestion = currentQuestion +1;
-
-    //call the display functions
-    displayAnswers();
-    displayQuestions();
-}
+    else {
+        timeLeft -= 15;
+        newTime.textContent = "Incorrect";
+        setTimeout(function () {
+            newTime.textContent = "";
+            displayAnswers();
+            displayQuestions();
+        }, 1000)
+    }
+    currentQuestion = currentQuestion + 1;
+};
 
 
 //WHEN I answer a question incorrectly
@@ -109,13 +116,13 @@ function authenticate() {
 
 //WHEN all questions are answered or the timer reaches 0
 //THEN the game is over
-function endGame () {
+function endGame() {
 
 };
 
 //WHEN the game is over
 //THEN I can save my initials and score
-function highScore () {
+function highScore() {
 
 }
 startBtn.addEventListener("click", startGame);
